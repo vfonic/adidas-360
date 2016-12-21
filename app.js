2d12fb092a8a6ee5c6c7147aadddc26d41581c61
Viktor Fonic <viktor.fonic@gmail.com>
2016-12-21 11:53:34 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('#spinner').spritespin({
+		// generate an array of image urls.
+		// this is a helper function that takes a {frame} placeholder
+		source: SpriteSpin.sourceArray('images/{frame}.png', {
+			// this ramge of numbers is interpolated into the {frame} placeholder
+			frame: [1,8],
+			// the frame placeholder will be padded with leading '0' up to the number of 'digits'
+			digits: 1
+		}),
+		// Specify the display width and height of the frame.
+		// Optionally the size of the container can be defined with CSS.
+		width: 522,
+		height: 552,
+		// Sense controls the direction and speed of the animation for mouse/touch interactions.
+		// Here a negative value is chosen to invert the rotation, so the animation 'follows' the drag direction.
+		// Values towards 0 will slow the animation down.
+		// sense: -1
+	});
+});
